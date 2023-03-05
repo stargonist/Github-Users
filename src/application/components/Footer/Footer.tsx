1582509f86dfa7f9cc4f5ad8d4e469ab9aa0aa08
Erick Ramirez <erickgiber7@gmail.com>
2023-03-05 02:06:24 -0400

    rename

--- src/application/components/Footer/Footer.tsx
+++ src/application/components/Footer/Footer.tsx
@@ -3,13 +3,13 @@ import styles from "./Footer.module.css";
 import logo from "../../../assets/codenet-team.png"
 
 export const Footer = () => {
-    const codenetTeamPage = import.meta.env.VITE_CODENET_TEAM_PAGE;
+    const stargonistWeb = import.meta.env.VITE_STARGONIST_WEB;
 
     return (
         <footer id={styles.footer}>
-            <Link target="_blank" id={styles.logo} to={codenetTeamPage}>
-                <img src={logo} alt="Codenet Team Logo"/>
-                <strong> Codenet Team </strong>
+            <Link target="_blank" id={styles.logo} to={stargonistWeb}>
+                <img src={logo} alt="Stargonist Logo"/>
+                <strong> Stargonist Team </strong>
             </Link>
         </footer>
     )
